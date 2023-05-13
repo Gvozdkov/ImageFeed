e5e6f76f25e27feec2bd356eeadfdb3bb33c85f2
onePoint <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>
2023-05-13 17:43:55 +0300

    Изменен StatusBar с черного на белый

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -12,11 +12,12 @@ class ImagesListViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     private let photosNames: [String] = Array(0..<20).map{ "\($0)" }
-    
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
     }
     
     private lazy var dateFormatter: DateFormatter = {
