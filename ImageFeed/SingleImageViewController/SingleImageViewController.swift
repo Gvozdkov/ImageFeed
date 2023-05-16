3d8d9fe9150ac295f03c06c24389f0a2a30c948e
onePoint <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>
2023-05-16 23:29:26 +0300

    В Main  добавлен новый контроллер Single Image View Controller для просмотра отдельного изображения

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Алексей Гвоздков on 16.05.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
