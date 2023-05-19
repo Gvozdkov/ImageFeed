570a745818e89c27df25a205b460c2e660c2db2e
onePoint <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>
2023-05-19 09:15:00 +0300

    В Single Image View Controller используется показ модального окна, т. е. картинка занимает весь экран и добавлена кнопка возврата

--- ImageFeed/SingleImageViewController/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -21,4 +21,7 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    @IBAction func didTapBackButton(_ sender: Any) {
+         dismiss(animated: true, completion: nil)
+    }
 }
