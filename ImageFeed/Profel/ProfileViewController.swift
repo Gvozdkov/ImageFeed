9bc4294b4e07b88d62035a5f93837d2024021972
onePoint <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>
2023-05-23 07:17:02 +0300

    1. Main -> Profile Scene удалены все элементы, добавленные в Storyboard. 2. Все элементы были сверстаны в ProfileViewController без использования Storyboard. 3. Исправлены цвета в Assets.

--- ImageFeed/Profel/ProfileViewController.swift
+++ ImageFeed/Profel/ProfileViewController.swift
@@ -7,14 +7,77 @@
 
 import UIKit
 final class ProfileViewController: UIViewController {
-    @IBOutlet var avatarImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var loginNameLabel: UILabel!
-    @IBOutlet var descriptionLabel: UILabel!
+    private var viewProfileImage = UIImageView()
+    private var labelNameProfile = UILabel()
+    private var loginNameLabel = UILabel()
+    private var descriptionLabel = UILabel()
+    private var buttonLogout = UIButton()
     
-    @IBOutlet var logoutButton: UIButton!
-    
-    @IBAction func didTapLogoutButton(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingsViewController()
     }
+    
+}
+
+private extension ProfileViewController {
+    func settingsViewController() {
+        view = UIView()
+        view.backgroundColor = UIColor(named: "YP Black")
+        
+        let imageProfile = UIImage(named: "Profile")
+        let imageButton = UIImage(named: "logoutButton")
+        
+        viewProfileImage = UIImageView(image: imageProfile)
+        viewProfileImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(viewProfileImage)
+        
+        labelNameProfile.text = "Екатерина Новикова"
+        labelNameProfile.textColor = UIColor(named: "YP White")
+        labelNameProfile.font = UIFont.systemFont(ofSize: 23)
+        labelNameProfile.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(labelNameProfile)
+        
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.textColor = UIColor(named: "YP Gray")
+        loginNameLabel.font = UIFont.systemFont(ofSize: 13)
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginNameLabel)
+        
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = UIColor(named: "YP White")
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        buttonLogout = UIButton.systemButton(with: imageButton!, target: self, action: #selector(didTapLogoutButton))
+        buttonLogout.tintColor = UIColor(named: "YP Red")
+        buttonLogout.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonLogout)
+        
+        
+        
+        NSLayoutConstraint.activate([
+            viewProfileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            viewProfileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            viewProfileImage.heightAnchor.constraint(equalToConstant: 70),
+            viewProfileImage.widthAnchor.constraint(equalToConstant: 70),
+            
+            labelNameProfile.topAnchor.constraint(equalTo: viewProfileImage.bottomAnchor, constant: 8),
+            labelNameProfile.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            loginNameLabel.topAnchor.constraint(equalTo: labelNameProfile.bottomAnchor, constant: 8),
+            loginNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            buttonLogout.centerYAnchor.constraint(equalTo: viewProfileImage.centerYAnchor),
+            buttonLogout.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26)
+        ])
+    }
+    
+    
+    @objc func didTapLogoutButton() { }
     
 }
