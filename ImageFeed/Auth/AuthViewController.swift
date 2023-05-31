523e40184b3eedc8691c07b59f1cbaf2d6523883
onePoint <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>
2023-05-31 17:51:25 +0300

    1. Создан AuthViewController, добавлен Image View и Button. 2, AuthViewController настроен согласно дизайну (https://www.figma.com/file/HyDfKh5UVPOhPZIhBqIm3q/Image-Feed-(YP)?type=design&node-id=334-4892&t=DjPogwfZoDqeh4Hp-0)

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Алексей Гвоздков on 31.05.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+
+}
